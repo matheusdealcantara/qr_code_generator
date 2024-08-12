a69b6d44509d9f01aa849ec27eb2f8faa8c569af
matheusdealcantara <dealcantaramatheus1@gmail.com>
2024-08-12 13:44:36 -0300

    Added navbar and login/register forms

--- app.py
+++ app.py
@@ -3,9 +3,11 @@ from io import BytesIO
 
 import qrcode
 from cs50 import SQL
-from flask import Flask, render_template, request, send_file
-from flask_session import Session
+from flask import Flask, render_template, request, send_file, session
 from werkzeug.security import check_password_hash, generate_password_hash
+
+from flask_session import Session
+from helpers import apology, login_required
 
 IMG_DIR = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static', 'img')
 
@@ -14,13 +16,25 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
-@app.route('/', methods=['GET', 'POST'])	
+db = SQL("sqlite:///qr.db")
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
+@app.route('/', methods=['GET', 'POST'])
+@login_required	
 def hello_world():
     if request.method == 'POST':
         data = request.form.get('url')
 
         if not data:
-            return render_template('index.html', error='Please enter a URL')
+            return apology('You must provide a URL', 403)
 
         qr = qrcode.QRCode(
             version=1,
@@ -42,6 +56,7 @@ def hello_world():
 
 
 @app.route('/generate', methods=['POST'])
+@login_required
 def generate():
     data = request.form.get('data')
 
@@ -60,3 +75,55 @@ def generate():
     img_io.seek(0)
 
     return send_file(img_io, mimetype='image/png')
+
+
+@app.route('/history')
+@login_required
+def history():
+    rows = db.execute('SELECT * FROM qr WHERE user_id = :user_id', user_id=session['user_id'])
+    return render_template('history.html', rows=rows)
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+        confirmation = request.form.get('confirmation')
+
+        if not username:
+            return apology('You must provide a username', 403)
+        if not password:
+            return apology('You must provide a password', 403)
+        if password != confirmation:
+            return apology('Passwords do not match', 403)
+
+        hash = generate_password_hash(password)
+        db.execute('INSERT INTO users (username, hash) VALUES (:username, :hash)', username=username, hash=hash)
+
+        return render_template('login.html')
+    else:
+        return render_template('register.html')
+    
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
+
+        if not username:
+            return apology('You must provide a username', 403)
+        if not password:
+            return apology('You must provide a password', 403)
+
+        rows = db.execute('SELECT * FROM users WHERE username = :username', username=username)
+
+        if len(rows) != 1 or not check_password_hash(rows[0]['hash'], password):
+            return apology('Invalid username and/or password', 403)
+
+        session['user_id'] = rows[0]['id']
+
+        return render_template('index.html')
+    else:
+        return render_template('login.html')
